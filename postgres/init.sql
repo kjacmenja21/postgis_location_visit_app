a1d9ed0d4a2e9a68163df8b85bd08c0b47a18642
kjacmenja21 <karlojacmenjak@gmail.com>
2025-01-02 23:02:26 +0100

    Move init.sql to postgres/

--- postgres/init.sql
+++ postgres/init.sql
@@ -36,3 +36,16 @@ CREATE TRIGGER datum_posjeta_trigger
 BEFORE INSERT ON lokacije
 FOR EACH ROW
 EXECUTE FUNCTION postavi_datum_posjeta();
+
+-- Create a function to insert markers
+CREATE OR REPLACE FUNCTION insert_marker(
+    naziv TEXT,
+    opis TEXT,
+    lat DOUBLE PRECISION,
+    lon DOUBLE PRECISION
+) RETURNS VOID AS $$
+BEGIN
+    INSERT INTO lokacije (naziv, opis, geometrija)
+    VALUES (naziv, opis, ST_SetSRID(ST_MakePoint(lon, lat), 4326));
+END;
+$$ LANGUAGE plpgsql;
